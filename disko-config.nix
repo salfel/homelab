bb4f7b291d3946a0b41f862e9b08bc5f36ab4f0f
Felix Salcher <dev.felix.salcher@gmail.com>
2024-11-20 16:18:33 +0100

    updated boot partition

--- disko-config.nix
+++ disko-config.nix
@@ -7,18 +7,15 @@
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
             ESP = {
-              size = "1G";
+              priority = 1;
+              start = "1M";
+              end = "128M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
